197a559bea647ccefa71a2a604d1a4f75f859c3f
Rui Almeida <ruial@outlook.com>
2016-02-01 18:28:57 +0000

    Added function to get country code

--- tor-country.sh
+++ tor-country.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 # Shell script to change Tor exit node country
+
+get_country_code() {
+    country=$(grep -i "$1" data.csv)
+    if [ "$country" ]
+    then
+    
+        lines=$(echo "$country" | wc -l)
+        if [ $lines -eq 1 ]
+        then
+            country_code=$(echo $country | rev | cut -d "," -f1 | rev)
+        else
+            echo -e "Multiple countries matched:\n$country"
+            return 1
+        fi
+        
+    else
+        echo "Could not find country."
+        return 1
+    fi
+    
+    return 0
+}
+
+
+get_country_code $1
+echo $country_code
