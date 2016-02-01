5cc318d6a7243b7debaee4e1ce12ace7af6bf8d7
Rui Almeida <ruial@outlook.com>
2016-02-01 18:54:21 +0000

    Added function to get torrc path

--- tor-country.sh
+++ tor-country.sh
@@ -23,6 +23,34 @@ get_country_code() {
     return 0
 }
 
+get_torrc_path(){
+    torrc_path=$(find / -type f -iwholename "*/Browser/TorBrowser/Data/Tor/torrc" 2>/dev/null)
+    if [ "$torrc_path" ]
+    then
+        
+        lines=$(echo "$torrc_path" | wc -l)
+        if [ $lines -gt 1 ]
+        then
+            counter=0;
+            for path in $torrc_path
+            do
+                counter=$((counter + 1))
+                echo "$counter- $path"
+            done
+            read -p "Chose the correct path: " op
+            torrc_path=$(echo "$torrc_path" | head -$op | tail -1)
+        fi
+        
+    else
+        echo "The torrc file was not found."
+        return 1
+    fi
+    
+    return 0
+}
+
 
 get_country_code $1
 echo $country_code
+get_torrc_path
+echo $torrc_path
